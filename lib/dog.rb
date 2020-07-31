170f04dd6bb5c190d412be267669ed59425b91a7
Leslie G <lesliegonzalez@me.com>
2020-07-31 22:06:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,17 @@
 class Dog
-  
   attr_accessor :name
 
   @@all = []
-  
+
   def initialize(name)
     @name = name
     save
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.print_all
     puts @@all.map{ |dog| dog.name }
   end

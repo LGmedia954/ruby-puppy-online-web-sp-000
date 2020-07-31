d78f6cbe1f526db33caf8d1a37b0a319075b3e17
Leslie G <lesliegonzalez@me.com>
2020-07-31 21:37:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,16 @@
 class Dog
   
+  attr_accessor :name
+  
   @@dog_count = 0
   
-  def initialize
+  def initialize(name)
     @@dog_count += 1
     
   end
+  
+  def self.count
+    @@dog_count 
   
   
   
